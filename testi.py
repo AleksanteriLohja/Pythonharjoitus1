accd4e35f9417d68a363838b3ae91f08719a9512
AleksanteriLohja <97512082+AleksanteriLohja@users.noreply.github.com>
2022-01-11 14:39:22 +0200

    Luotu eka harjoitus

---
+++ testi.py
@@ -0,0 +1,11 @@
+# Ensinmäinen harjoitus muuttujat, näppäimstä ja näyttö
+
+# Määritellään muuttujat
+
+first_name = input('Mikä on etunimesi? ')
+favourite_place = input('Mikä on suosikkipaikkasi? ')
+nickname = input('Mikä on lempinimesi? ') 
+year_of_birth = input('Minä vuonna olet syntynyt? ')
+
+# Tulostetaan tiedot ruudulle
+print(first_name, 'olet syntynyt', year_of_birth)
